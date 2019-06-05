7067def388becc8e7a358de054c8a3a768e1e883
Khoa Phan <khoaphan89@gmail.com>
2019-06-05 16:20:12 -0400

    Run terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "min_capacity" {
-  type = number
+  type        = number
   description = "Minimum number of instances that must be active in the ECS cluster"
 }
 
 variable "max_capacity" {
-  type = number
+  type        = number
   description = "Maximum number of instances that can be active in the ECS cluster"
 }
 
@@ -19,23 +19,23 @@ variable "ecs_cluster_service_name" {
 }
 
 variable "scale_up_policy_name" {
-  type = string
+  type        = string
   description = "Name of the scaling up policy"
 }
 
 variable "scale_up_adjustment" {
-  type = number
+  type        = number
   description = "Number of instances to add for a scaling up action"
-  default = 1
+  default     = 1
 }
 
 variable "scale_down_policy_name" {
-  type = string
+  type        = string
   description = "Name of the scaling down policy"
 }
 
 variable "scale_down_adjustment" {
-  type = number
+  type        = number
   description = "Number of instances to remove for a scaling down action"
-  default = -1
+  default     = -1
 }
