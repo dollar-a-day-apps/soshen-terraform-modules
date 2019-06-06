4fe602ceea08fd881859b2a63a6d9bf48637f77f
Khoa Phan <khoaphan89@gmail.com>
2019-06-06 11:13:45 -0400

    Update map variable types

--- variables.tf
+++ variables.tf
@@ -24,23 +24,23 @@ variable "resource_environment_tag" {
 }
 
 variable "target_group" {
-  type        = map(any)
+  type        = map(string)
   description = "Target groups are forwarded requests from the load balancer. Accepts these props: protocol (HTTP or HTTPS), port, stickiness_enabled, health_check_path"
 }
 
 variable "listener" {
-  type        = map(any)
+  type        = map(string)
   description = "Listeners handle requests on specific load balancer ports. Accepts these props: port, protocol (HTTP or HTTPS), certificate_arn"
 }
 
 variable "cidr_block_security_groups" {
-  type        = list(map(any))
+  type        = list(map(string))
   description = "Security groups for CIDR blocks. Accept these props: from_port, to_port, protocol, cidr_block, ipv6_cidr_block"
   default     = []
 }
 
 variable "source_security_groups" {
-  type        = list(map(any))
+  type        = list(map(string))
   description = "Security groups for source security group ids. Accept these props: type, from_port, to_port, protocol, source_security_group_id"
   default     = []
 }
