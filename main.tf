ae160c1ac49972bf7f9952bea03171f24da20295
Khoa Phan <khoaphan89@gmail.com>
2019-06-06 13:34:34 -0400

    Change variable name to reflect list of records

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ data "aws_route53_zone" "soshen" {
 
 # Allows us to create records (rules) for routing domain traffic
 resource "aws_route53_record" "soshen" {
-  count   = length(var.route53_record)
+  count   = length(var.route53_records)
   zone_id = data.aws_route53_zone.soshen.host_id
-  type    = var.route53_record[count.index].type
-  name    = var.route53_record[count.index].name
+  type    = var.route53_records[count.index].type
+  name    = var.route53_records[count.index].name
 
   alias {
-    evaluate_target_health = var.route53_record[count.index].alias_evaluate_target_health
+    evaluate_target_health = var.route53_records[count.index].alias_evaluate_target_health
 
     # Accessing node.soshen.io via a browser will send the visitor to our load balancer
-    zone_id = var.route53_record[count.index].alias_zone_id
-    name    = var.route53_record[count.index].alias_name
+    zone_id = var.route53_records[count.index].alias_zone_id
+    name    = var.route53_records[count.index].alias_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "route53_record" {
+variable "route53_records" {
   type        = list(map(string))
   description = "Records allow us to manage how traffic to our domain is routed. Accepts these props: type, name, alias_evaluate_target_health, alias_zone_id, alias_name"
 }
