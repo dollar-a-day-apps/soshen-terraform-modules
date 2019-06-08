af3658124ed898743d91c00737650bd685046af4
Khoa Phan <khoaphan89@gmail.com>
2019-06-08 11:53:28 -0700

    Update variable description to reflect public_subnet_ids as indie variable

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "ecs_cluster_name" {
 
 variable "ecs_service" {
   type        = map(string)
-  description = "ECS service which manages task container instances. Accepts the following props: desired_count, name, container_port, container_name, target_group_arn, public_subnet_ids"
+  description = "ECS service which manages task container instances. Accepts the following props: desired_count, name, container_port, container_name, target_group_arn"
 }
 
 variable "container_security_group" {
