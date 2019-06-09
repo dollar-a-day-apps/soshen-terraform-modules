483341d96d83ec3ed04b1ead06475c8dae4fa347
Khoa Phan <khoaphan89@gmail.com>
2019-06-09 11:53:01 -0700

    Add tags variable

--- main.tf
+++ main.tf
@@ -7,20 +7,21 @@ resource "aws_security_group" "security_group" {
   }
 
   tags = {
-    Name        = var.resource_name_tag
-    Environment = var.resource_environment_tag
+    Name        = var.tags.Name
+    Environment = var.tags.Environment
+    Description = var.tags.Description
   }
 }
 
 resource "aws_security_group_rule" "cidr_block" {
-  security_group_id        = aws_security_group.security_group.id
+  security_group_id = aws_security_group.security_group.id
 
-  count                    = length(var.cidr_block_security_group_rules)
-  type                     = var.cidr_block_security_group_rules[count.index][type]
-  protocol                 = var.cidr_block_security_group_rules[count.index][protocol]
-  from_port                = var.cidr_block_security_group_rules[count.index][from_port]
-  to_port                  = var.cidr_block_security_group_rules[count.index][to_port]
-  cidr_blocks = [var.cidr_block_security_group_rules[count.index][cidr_block]]
+  count            = length(var.cidr_block_security_group_rules)
+  type             = var.cidr_block_security_group_rules[count.index][type]
+  protocol         = var.cidr_block_security_group_rules[count.index][protocol]
+  from_port        = var.cidr_block_security_group_rules[count.index][from_port]
+  to_port          = var.cidr_block_security_group_rules[count.index][to_port]
+  cidr_blocks      = [var.cidr_block_security_group_rules[count.index][cidr_block]]
   ipv6_cidr_blocks = [var.cidr_block_security_group_rules[count.index][ipv6_cidr_block]]
 
   lifecycle {
@@ -29,7 +30,7 @@ resource "aws_security_group_rule" "cidr_block" {
 }
 
 resource "aws_security_group_rule" "source_security_group" {
-  security_group_id        = aws_security_group.security_group.id
+  security_group_id = aws_security_group.security_group.id
 
   count                    = length(var.source_security_group_rules)
   type                     = var.source_security_group_rules[count.index][type]
--- variables.tf
+++ variables.tf
@@ -1,16 +1,21 @@
 variable "vpc_id" {
-  type = string
+  type        = string
   description = "ID of the VPC"
 }
 
 variable "cidr_block_security_group_rules" {
-  type = list(map(string))
+  type        = list(map(string))
   description = "Security group rules for moderating ingress/egress via CIDR blocks. Accepts the following props: type, protocol, from_port, to_port, cidr_block, ipv6_cidr_block"
-  default = []
+  default     = []
 }
 
 variable "source_security_group_rules" {
-  type = list(map(string))
+  type        = list(map(string))
   description = "Security group rules for moderating ingress/egress via external security group IDs. Accepts the following props: type, protocol, from_port, to_port, source_security_group_id"
-  default = []
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Resource tags. Accepts the following props: Name, Environmnet, Description"
 }
