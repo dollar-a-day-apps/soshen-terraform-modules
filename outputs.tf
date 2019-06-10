24363559ed27752627af9d5ef89b06fde6638697
Khoa Phan <khoaphan89@gmail.com>
2019-06-10 16:21:26 -0700

    Add cache_nodes output and fix security group reference

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,13 @@
+# Used for write operations
 output "primary_endpoint_address" {
   value = aws_elasticache_replication_group.redis.primary_endpoint_address
 }
 
+# Used for read operations (upon failure, a new node will be promoted to primary)
+output "cache_nodes" {
+  value = aws_elasticache_cluster.redis.cache_nodes
+}
+
 output "security_group_id" {
-  value = aws_security_group.redis.id
+  value = module.security_group.id
 }
